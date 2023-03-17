f024cb28c2fd7ecc64cc8e28b843d54ed150ee91
unknown <sagarmikeylevi@gmail.com>
2023-03-17 22:25:25 +0530

    Learn about composition

--- src/App.js
+++ src/App.js
@@ -1,46 +1,36 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 function App() {
   const expenses = [
-
     {
-      id: 'e1',
+      id: "e1",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2023, 2, 28),
+      date: new Date(2023, 4, 21),
     },
     {
-      id: 'e2',
+      id: "e2",
       title: "Gym Expenses",
       amount: 29.67,
-      date: new Date(2023, 2, 28),
+      date: new Date(2022, 7, 21),
     },
     {
-      id: 'e3',
+      id: "e3",
       title: "Home Rent",
       amount: 24.67,
-      date: new Date(2023, 2, 28),
+      date: new Date(2020, 9, 16),
     },
-
+    {
+      id: "e4",
+      title: "Study Expenses",
+      amount: 27.67,
+      date: new Date(2021, 3, 28),
+    },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
+      <Expenses item={expenses} />
     </div>
   );
 }
