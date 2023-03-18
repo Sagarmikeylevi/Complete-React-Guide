5ccfb792672d2e0e12f2634e12bf731bbe627c13
unknown <sagarmikeylevi@gmail.com>
2023-03-18 19:41:49 +0530

    Understing useState hook

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Card.css";
 
-function Card(props) {
+const Card = (props) => {
   const classes = "card " + props.className;
 
   return <div className={classes}>{props.children}</div>;
