edf17b2511ad89ab624ec64d70262b37447609b1
unknown <sagarmikeylevi@gmail.com>
2023-03-18 18:30:38 +0530

    Completed the Componenets Part

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,7 @@
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 function ExpenseItem(props) {
   return (
     <Card className="expense-item">
